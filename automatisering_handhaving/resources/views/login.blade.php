d056c75e47219195f6dd7fd15dd0556f0187ca89
kimi <158205@student.horizoncollege.nl>
2025-04-15 14:06:57 +0200

    added: basic styling to login form

--- automatisering_handhaving/resources/views/login.blade.php
+++ automatisering_handhaving/resources/views/login.blade.php
@@ -5,24 +5,25 @@
     <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
 <body>
-    @auth
-        <h1>Je bent al ingelogd</h1>
-        <!-- log out button incase user is logged in -->
-        <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button type="submit">Uitloggen</button>
-        </form>
-    @else
-        <h1>Login</h1>
-        <!-- login form -->
-        <form action="{{ route('login') }}" method="POST">
-            @csrf
-            <label for="email">Email:</label>
-            <input type="email" name="email" id="email" required>
-            <label for="password">Wachtwoord:</label>
-            <input type="password" name="password" id="password" required>
-            <button type="submit">Inloggen</button>
-        </form>
-    @endauth
+    <div class="loginform">
+        @auth
+            <h1>Je bent al ingelogd</h1>
+            <!-- log out button incase user is logged in -->
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit">Uitloggen</button>
+            </form>
+        @else
+            <!-- login form -->
+            <form action="{{ route('login') }}" method="POST">
+                @csrf
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" required>
+                <label for="password">Wachtwoord:</label>
+                <input type="password" name="password" id="password" required>
+                <button type="submit">Inloggen</button>
+            </form>
+        @endauth
+    </div>
 </body>
 </html>
