3098d47b9e1f844607d76b8eb06d4934193686e0
kimi <158205@student.horizoncollege.nl>
2025-05-22 15:25:42 +0200

    updated: account page now has content

--- automatisering_handhaving/resources/views/account.blade.php
+++ automatisering_handhaving/resources/views/account.blade.php
@@ -1,14 +1,34 @@
 @extends('layouts.app')
 
-{{-- @push('styles')
-<link rel="stylesheet" href="{{ asset('css/.css') }}">
-@endpush
-
-@push('scripts')
-<script src="{{ asset('js/.js') }}"></script>
-@endpush --}}
-
-
 @section('title', 'Accountpagina')
 
+@push('styles')
+<link rel="stylesheet" href="{{ asset('css/account.css') }}">
+@endpush
+
 @section('content')
+
+
+<div class="account-page">
+    <div class="account-card">
+        <div class="text-center">
+            <div class="account-avatar">
+                <i class="bi bi-person"></i>
+            </div>
+            <h3 class="mb-3">Mijn account</h3>
+        </div>
+        {{-- show relevant information about the user --}}
+        <div class="account-info">
+            <p><strong>Naam:</strong> {{ Auth::user()->name }}</p>
+            <p><strong>Gebruikersnaam:</strong> {{ Auth::user()->username }}</p>
+            <p><strong>Email:</strong> {{ Auth::user()->email }}</p>
+            <p><strong>Klas:</strong> 
+                @php
+                    $group = \App\Models\Groups::find(Auth::user()->group_id);
+                @endphp
+                {{ $group ? $group->name : '-' }}
+            </p>
+        </div>
+    </div>
+</div>
+@endsection
